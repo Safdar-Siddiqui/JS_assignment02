b860295aeb2e686dc3195c100db14bdf26f36d89
Safdar-Siddiqui <safdarsiddiqui25@gmail.com>
2023-09-23 16:44:09 +0500

    assignment-02

---
+++ app.js
@@ -0,0 +1,41 @@
+// ----1----
+var userName;
+
+// ----2----
+let myName ="Safdar Siddiqui";
+
+
+// ----3----
+let message;
+message = "Hello World !";
+alert(message);
+
+
+// ----4----
+let studentName= "Safdar "
+let studentAge = 15;
+let course= " Web Development"
+alert("Welcome " + studentName);
+alert(studentName + " is " + studentAge + " years old");
+alert(studentName + " is studying " + course);
+
+
+// ----6----
+let myEmailAddress="safdarsiddiqui25@gmail.com";
+alert("my email address is " + myEmailAddress);
+
+
+
+// ----7----
+let book ="A smarter way to learn JavaScript";
+alert("I am trying to learn from the Book " + book);
+
+
+//----8----
+let msg="Yeah! I cant write HTML through Javascript"
+document.write(msg);
+
+
+// ----9----
+let string="▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬";
+alert(string);
